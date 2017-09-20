5a5cbb281ed47f3b353681ca9318ec2e467d810f
Austin Fite <akfite@gmail.com>
2017-09-19 23:30:05 -0400

    Rough class skeleton is in place

--- AutoCompleteEditbox.m
+++ AutoCompleteEditbox.m
@@ -1,12 +1,42 @@
-classdef AutoCompleteEditbox
+classdef AutoCompleteEditbox < matlab.mixin.SetGet
     %AUTOCOMPLETEEDITBOX Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
+    properties (SetAccess = protected)
+        BackgroundColor = [0.94 0.94 0.94]
+        Enable      = true
+        FontAngle   = 'normal'
+        FontName    = 'Helvetica'
+        FontSize    = 10
+        FontWeight  = 'normal'
+        HorizontalAlignment = 'left'
+        Parent      = []
+        Position    = [20 20 200 26]
+        String      = ''
+        Tag         = ''
+        UserData    = []
+        Visible     = true
     end
     
+    properties (SetAccess = protected) % TODO: change to protected (set & get)
+        jSearchText
+        hSearchText
+        jComboBox
+        hComboBox
+    end
+    
+    %% Constructor
+    methods
+        function this = AutoCompleteEditbox()
+        end
+    end
+    
+    %% Public interface
     methods
     end
     
+    %% Private methods
+    methods (Access = private)
+    end
 end
 
