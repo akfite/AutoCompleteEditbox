dc09674da08f764705214c7f20b90502fc89789c
Austin Fite <akfite@gmail.com>
2018-03-04 15:47:04 -0500

    Fully implemented param/value constructor

--- AutoCompleteEditbox.m
+++ AutoCompleteEditbox.m
@@ -1,6 +1,7 @@
 classdef AutoCompleteEditbox < matlab.mixin.SetGet
-    %AUTOCOMPLETEEDITBOX Summary of this class goes here
-    %   Detailed explanation goes here
+    %AUTOCOMPLETEEDITBOX An editbox with a built-in auto-complete feature.
+    %   h = AUTOCOMPLETEEDITBOX(prop, value, ...) creates an editbox as a child of
+    %   PARENT with 
     
     properties
         BackgroundColor = [255 255 255]
@@ -11,14 +12,14 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
         FontWeight  = 'normal'
         FontColor   = [0 0 0]
         HorizontalAlignment = 'left'
-        MatchFontColor  = [0 0 255]
+        MatchFontColor = [0 0 255]
     end
     properties (SetAccess = protected)
         Matches % i want the prop list alphabetical but i cba overloading disp() right now
     end
     properties
         Parent      = []
-        Position    = [200 200 200 26]
+        Position    = [20 20 200 26]
         String      = ''
         TooltipString = ''
         Units       = 'pixels'
@@ -26,7 +27,7 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
         Visible     = true
     end
     
-    properties (SetAccess = protected) % DEBUG: will change to fully protected
+    properties (Access = protected) % DEBUG: will change to fully protected
         jTextField
         hTextField
         jComboBox
@@ -34,25 +35,22 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
     end
     
     events (NotifyAccess = protected)
-        EnterKeyPress
+        EnterKeyPressed
     end
     
     %% Constructor
     methods
-        function this = AutoCompleteEditbox(parent, varargin)
-            if nargin < 1
-                parent = gcf;
-            end
+        function this = AutoCompleteEditbox(varargin)
+            params = getInitialConfiguration(this, varargin{:});
             
-            % create the uicontrols
-            createObjectInFigure(this, parent);
+            % create the uicontrols with jTextField and jComboBox defaults
+            createObjectInFigure(this, params.parent);
             
-            % set all properties to their default values
-            refreshProperties(this);
-            
-            % now overwrite the default props with whatever was provided by the user
-            for i = 1:2:length(varargin)
-                this.(varargin{i}) = varargin{i+1};
+            % initialize settings for the AutoCompleteEditbox wrapper class
+            props = fieldnames(params);
+            for i = 1:length(props)
+                prop = props{i};
+                set(this, prop, params.(prop));
             end
         end
     end
@@ -61,10 +59,11 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
     methods (Access = private)
         function createObjectInFigure(this, parent)
             % create the JComboBox first so that it appears hidden behind the textbox
-            [jComboBox, hComboBox] = javacomponent(javax.swing.JComboBox, [], parent);
+            jComboBox = javaObjectEDT(javax.swing.JComboBox);
+            [jComboBox, hComboBox] = javacomponent(jComboBox, [], parent);
             
             % now create the search field so that it sits on top of the combobox
-            jTextField = javax.swing.JTextField;
+            jTextField = javaObjectEDT(javax.swing.JTextField);
             [jTextField, hTextField] = javacomponent(jTextField, [], parent);
             
             % update object state
@@ -76,22 +75,21 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
             % setup keypress callback functions
             jTextHCallback = handle(jTextField,'CallbackProperties');
             set(jTextHCallback, 'KeyPressedCallback', @(src,evnt) keyRoutingFcn(this, src, evnt));
-            
-            this.Parent = parent;
         end
         
-        function keyRoutingFcn(this, ~, evnt)
+        function keyRoutingFcn(this, src, evnt)
             modifiers = get(evnt, 'Modifiers'); % 1 = shift, 2 = ctrl, 8 = alt.  sum for combinations
             if ~ismember(modifiers, [0 1]) % only allows no modifier or shift to pass through
                 return; % prevents trigger on CTRL+C/V/A
             end
             
             keyCode = get(evnt,'ExtendedKeyCode');
+            disp(src)
             
             switch keyCode
                 case 10 % ENTER will select the current item from the jComboBox
                     setSelectedItem(this);
-                    notify(this, 'EnterKeyPress');
+                    notify(this, 'EnterKeyPressed');
                 case 27 % ESC hides the popup
                     this.jComboBox.hidePopup;
                 case {38 40} % UP/DOWN ARROW scrolls through jComboBox suggestions
@@ -101,6 +99,7 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
                     end
                     
                     selectedIndex = int32(this.jComboBox.SelectedIndex);
+                    fprintf('Original Index: %d\n', selectedIndex);
                     
                     if keyCode == 38
                         % UP ARROW: move selection up but don't let it go negative
@@ -112,8 +111,9 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
                         selectedIndex = selectedIndex + 1;
                         selectedIndex(selectedIndex > (itemCount-1)) = itemCount-1;
                     end
-                    
+                    fprintf('New Index: %d\n', selectedIndex);
                     this.jComboBox.setSelectedIndex(selectedIndex);
+                    fprintf('Post Set: %d\n', int32(this.jComboBox.SelectedIndex));
                 otherwise
                     autoComplete(this)
             end
@@ -130,29 +130,27 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
                 matchedText = regexpi(this.CompletionList, textInput, 'match','once');
             end
             
-            matchIndex = ~cellfun('isempty', matchedText);
-            
-            if ~any(matchIndex)
-                this.jComboBox.hidePopup;
-            end
+            this.Matches = ~cellfun('isempty', matchedText);
             
             % highlight the matched text in the popup
-            matchedText = matchedText(matchIndex);
-            matchList = this.CompletionList(matchIndex);
-            matchFormatted = strcat(...
-                sprintf('<b><font color="rgb(%d,%d,%d)">',this.MatchFontColor), ...
-                matchedText, ...
-                '</b></font>');
-            matchList = strrep(matchList, matchedText, matchFormatted);
-            matchList = strcat('<html>',matchList,'</html>');
-            
-            try
-                this.jComboBox.setModel(javax.swing.DefaultComboBoxModel(matchList));
-                this.jComboBox.showPopup;
-            catch ex
+            if any(this.Matches)
+                matchedText = matchedText(this.Matches);
+                matchList = this.CompletionList(this.Matches);
+                matchFormatted = strcat(...
+                    sprintf('<b><font color="rgb(%d,%d,%d)">',this.MatchFontColor), ...
+                    matchedText, ...
+                    '</b></font>');
+                matchList = strrep(matchList, matchedText, matchFormatted);
+                matchList = strcat('<html>',matchList,'</html>');
+
+                try
+                    this.jComboBox.setModel(javax.swing.DefaultComboBoxModel(matchList));
+                    this.jComboBox.showPopup;
+                catch ex
+                end
+            else
+                this.jComboBox.hidePopup;
             end
-            
-            this.Matches = matchIndex;
         end
         
         function setSelectedItem(this, ~, ~) % give it 2 extra args so that it can be a callback
@@ -164,14 +162,58 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
             end
         end
         
-        function refreshProperties(this)
-            props = properties(this);
+        function paramStruct = getInitialConfiguration(this, varargin)
+            if isempty(varargin)
+                params = {};
+            else
+                % check that varargin is formatted correctly (parmeter/value pairs)
+                if mod(length(varargin),2)
+                    error('All input arguments must be param/value pairs.');
+                elseif any(~cellfun(@ischar, varargin(1:2:end)))
+                    error('All param inputs must be of type char.');
+                end
+                
+                varargin(1:2:end) = lower(varargin(1:2:end)); % all lowercase makes life ez
+                params = varargin(1:2:end);
+                values = varargin(2:2:end);
+            end
+            
+            props = lower(properties(this));
+            
+            if any(~ismember(params, props))
+                iBadParam = find(~ismember(params, props),1,'first');
+                error('''%s'' is not a property of class %s.', params{iBadParam}, class(this));
+            end 
+            
+            p = inputParser;
+            
+            % add the default props to the input parser list
             for i = 1:length(props)
-                try
-                    % setter functions actually apply the changes
-                    this.(props{i}) = this.(props{i});
+                prop = props{i};
+                switch prop
+                    case 'string'
+                        % 'String' is a special case because it requires the editbox to exist
+                        addOptional(p, 'string', '');
+                    case 'parent'
+                        % default is 'gcf', but that can spawn a new figure inadvertently...
+                        % so we're going to cheat and check ahead that 'parent is in in 
+                        % the arg list before setting 'gcf' as the default
+                        iParentParam = ismember('parent', params);
+                        if sum(iParentParam) == 1
+                            parentDefault = values{iParentParam};
+                        else
+                            % 'parent' not found in arg list: safe to use gcf
+                            parentDefault = gcf;
+                        end
+                        addOptional(p, 'parent', gcf);
+                    case 'matches'
+                        continue % protected prop
+                    otherwise
+                        addOptional(p, prop, get(this, prop))
                 end
             end
+            parse(p,varargin{:});
+            paramStruct = p.Results;
         end
     end
     
@@ -185,6 +227,9 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
     %% Accessors (setters)
     methods
         function set.BackgroundColor(this, value)
+            if ischar(value)
+                value = localConvertColorSpecToRGB(value);
+            end
             p = inputParser;
             addRequired(p, 'BackgroundColor', ...
                 @(x) validateattributes(x, {'numeric'},{'vector','numel',3,'>=',0,'<=',255}));
@@ -205,6 +250,22 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
             this.CaseSensitive = logical(value);
         end
         
+        function set.CompletionList(this, value)
+            p = inputParser;
+            addRequired(p, 'CompletionList', @(x) validateattributes(x, {'cell','string'},{'vector'}));
+            parse(p, value); % this is only to generate errors
+            
+            % force type to be a cellstr column vector
+            if isa(value, 'string')
+                value = {value{:}}'; % convert to cellstr
+            elseif isa(value, 'cell') && isrow(value)
+                value = value';
+            end
+                
+            this.CompletionList = unique(value);
+            autoComplete(this);
+        end
+        
         function set.Enabled(this, value)
             p = inputParser;
             addRequired(p, 'Enabled', @(x) validateattributes(x, {'logical','numeric'},{'scalar','binary'}));
@@ -217,6 +278,9 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
         end
         
         function set.FontColor(this, value)
+            if ischar(value)
+                value = localConvertColorSpecToRGB(value);
+            end
             p = inputParser;
             addRequired(p, 'FontColor', ...
                 @(x) validateattributes(x, {'numeric'},{'vector','numel',3,'>=',0,'<=',255}));
@@ -264,10 +328,13 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
             
             jAlignment = javax.swing.JTextField.(upper(value));
             this.jTextField.setHorizontalAlignment(jAlignment);
-            this.HorizontalAlignment = alignment;
+            this.HorizontalAlignment = value;
         end
         
         function set.MatchFontColor(this, value)
+            if ischar(value)
+                value = localConvertColorSpecToRGB(value);
+            end
             p = inputParser;
             addRequired(p, 'MatchFontColor', ...
                 @(x) validateattributes(x, {'numeric'},{'vector','numel',3,'>=',0,'<=',255}));
@@ -337,3 +404,26 @@ classdef AutoCompleteEditbox < matlab.mixin.SetGet
     end
 end
 
+function RGB = localConvertColorSpecToRGB(colorspec)
+    switch colorspec
+        case {'y','yellow'}
+            RGB = [1 1 0];
+        case {'m','magenta'}
+            RGB = [1 0 1];
+        case {'c','cyan'}
+            RGB = [0 1 1];
+        case {'r','red'}
+            RGB = [1 0 0];
+        case {'g','green'}
+            RGB = [0 1 0];
+        case {'b','blue'}
+            RGB = [0 0 1];
+        case {'w','white'}
+            RGB = [1 1 1];
+        case {'k','black'}
+            RGB = [0 0 0];
+        otherwise
+            error('''%s'' is not a MATLAB ColorSpec.', colorspec);
+    end
+    RGB = RGB * 255;
+end
